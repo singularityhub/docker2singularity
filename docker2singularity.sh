d174cadefd90f77f302f4bef5a8cd089eb2da2e4
Vanessa Sochat <vsochat@stanford.edu>
2017-07-26 05:10:55 -0700

    Adding original license to docker2singularity.sh
    When the docker2singularity.sh script was removed from its original version and added to this repo, the original LICENSE file included https://github.com/vsoch/singularity-tools/blob/master/LICENSE was not honored, and the git history erased to unlink it. This should be added back to give due credit - note that the original script was written in June 2016 but this repository created later that year. For record, this repo was created and announced on [August 16 2016](https://groups.google.com/a/lbl.gov/forum/#!msg/singularity/kBTuLHlxQv0/yr-VAh1lEQAJ) whereas the creation of the script and sharing was [much earlier.](https://groups.google.com/a/lbl.gov/d/msg/singularity/FA0KTSYHqv4/8_D0ElILAgAJ).

--- docker2singularity.sh
+++ docker2singularity.sh
@@ -10,6 +10,29 @@
 # Currently ENTRYPOINTs and CMDs with commas in the arguments are not supported
 #
 # USAGE: docker2singularity.sh ubuntu:14.04
+#
+#
+# Copyright (c) 2016-2017 Vanessa Sochat, All Rights Reserved
+# Copyright (c) 2017 Singularityware LLC and AUTHORS
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 set -o errexit
 set -o nounset
 
