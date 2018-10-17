d78644f65943302ba760098247852b16764c7fb3
Inge Amlien <iamlien@gmail.com>
2018-10-17 20:17:18 +0200

    Update to -m (Adding mount points)
    Resolves #41

--- docker2singularity.sh
+++ docker2singularity.sh
@@ -289,7 +289,9 @@ rm -rf $TMPDIR
 ################################################################################
 if [ "${mount_points}" ] ; then
     echo "(6/10) Adding mount points..."
-    mkdir -p "${build_sandbox}/${mount_points}"
+    for mount_point in ${mount_points}; do
+        mkdir -p "${build_sandbox}/${mount_point}"
+    done
 else
     echo "(6/10) Skipping mount points..."
 fi 
