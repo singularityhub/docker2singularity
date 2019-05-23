2e9bb9c5ebd883949c58591e928ce000947e14cd
Vanessa Sochat <vsochat@stanford.edu>
2019-05-23 18:40:28 -0400

    fixing mount points command for 2.3
    Signed-off-by: Vanessa Sochat <vsochat@stanford.edu>

--- docker2singularity.sh
+++ docker2singularity.sh
@@ -12,7 +12,7 @@
 # USAGE: docker2singularity.sh ubuntu:14.04
 #
 #
-# Copyright (c) 2016-2017 Vanessa Sochat, All Rights Reserved
+# Copyright (c) 2016-2019 Vanessa Sochat, All Rights Reserved
 # Copyright (c) 2017 Singularityware LLC and AUTHORS
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -187,9 +187,15 @@ rm -rf $TMPDIR
 ################################################################################
 ### Permissions ################################################################
 ################################################################################
-if [ "${mount_points}" ]; then
-echo "(6/9) Adding mount points..."
-singularity exec --writable --contain $new_container_name /bin/sh -c "mkdir -p ${mount_points}"
+if [ "${mount_points}" ] ; then
+    echo "(6/9) Adding mount points..."
+    for mount_point in ${mount_points}; do
+        singularity exec --writable --contain $new_container_name /bin/sh -c "mkdir -p ${mount_point}"
+    done
+else
+    echo "(6/10) Skipping mount points..."
+fi 
+
 else 
 echo "(6/9) Skipping mount points..."
 fi 
